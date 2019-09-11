0808ec03147de4ae4e644812b49e1f51f3fef584
Andrew E Slaughter <andrew.slaughter@inl.gov>
2019-09-11 17:08:39 -0600

    Skip SQA in python tests for now
    (refs #3)

--- scripts/sqa_check.py
+++ scripts/sqa_check.py
@@ -42,6 +42,7 @@ if __name__ == '__main__':
     cmd = ['git', 'fetch', opt.remote]
     subprocess.call(cmd)
 
+    opt.skip += 'python'
     count = mooseutils.sqa_check(opt.directory, opt.remote, opt.branch, opt.specs, opt.skip)
 
     if opt.duplicates:
